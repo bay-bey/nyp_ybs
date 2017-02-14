e68da57e5421ea2912915c490a0d998fcb89e08c
PauAna <PauAna>
2017-02-14 17:56:55 +0300

    14.02.2017 sınıflara giriş

---
+++ Siniflar/Siniflar/Kisi.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Siniflar
+{
+    class Kisi
+    {
+        public string adi;//kişinin adını saklar
+        public string soyadi;
+        public string gozRengi;
+        public int dogumYili;
+
+        public Kisi()//Varsayılan kurucu metod
+        {
+            dogumYili = 1990;
+        }
+
+        public Kisi(string adi, string soyadi, string gozRengi, int dogumYili)
+        {
+            this.adi = adi;
+            this.soyadi = soyadi;
+            this.gozRengi = gozRengi;
+            this.dogumYili = dogumYili;
+        }
+
+        public void yazdir()
+        {
+            //Console.WriteLine("Ben bir kişi sınıfıyım");
+            Console.WriteLine("Adım: {0}",adi);
+            Console.WriteLine("Soyadım: {0}", soyadi);
+            Console.WriteLine("Göz rengim: {0}", gozRengi);
+            Console.WriteLine("Doğum yılım: {0}", dogumYili);
+        }
+    }
+}
---
+++ Siniflar/Siniflar/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Siniflar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kisi k1 = new Kisi();//Kisi sınıfından k1 nesnesini oluştur
+            Kisi k2 = new Kisi();//Kisi sınıfından k2 nesnesini oluştur
+            k1.adi = "Şevket";
+            k1.soyadi = "Çakır";
+            k1.gozRengi = "Ela";
+            k2.adi = "Hüseyin";
+            k2.soyadi = "Kızılkaya";
+            k2.gozRengi = "Kahverengi";
+            k2.dogumYili = 1994;
+            k1.yazdir();//k1 nesnesinin yazdır metodunu çağır
+            k2.yazdir();//k2 nesnesinin yazdır metodunu çağır
+            Kisi k3 = new Kisi("Burak", "Atalay", "Kahverengi", 1994);
+            k3.yazdir();
+
+        }
+    }
+}
