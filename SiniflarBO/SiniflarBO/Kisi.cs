17b9da1aedf75fef65d4f17ab443e372314a81fc
PauAna <PauAna>
2017-02-16 10:06:21 +0300

    16.02.2017 derste işlenen kodlar

---
+++ SiniflarBO/SiniflarBO/Kisi.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiniflarBO
+{
+    class Kisi
+    {
+        public string ad;
+        public string soyad;
+        public int dogumYili;
+        public string gozRengi;
+
+        public Kisi()
+        {
+            dogumYili = 1990;
+        }
+
+        public Kisi(string ad, string soyad, int dogumYili, string gozRengi)
+        {
+            this.ad = ad;
+            this.soyad = soyad;
+            this.dogumYili = dogumYili;
+            this.gozRengi = gozRengi;
+        }
+
+        public void yazdir()
+        {
+            //Console.WriteLine("Kişi sınıfının yazdır metodu");
+            Console.WriteLine("Adım: {0}", ad);
+            Console.WriteLine("Soyadım: {0}", soyad);
+            Console.WriteLine("Doğum yılım: {0}",dogumYili);
+            Console.WriteLine("Göz rengim: {0}", gozRengi);
+        }
+    }
+}
---
+++ SiniflarBO/SiniflarBO/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiniflarBO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kisi k1 = new Kisi();
+            //Console.Write("Adınızı Girin: ");
+            //k1.ad = Console.ReadLine();
+            k1.ad = "Şevket";
+            k1.soyad = "Çakır";
+            k1.gozRengi = "Ela";
+            k1.yazdir();
+            Kisi k2 = new Kisi();
+            k2.ad = "Ertuğrul";
+            k2.soyad = "Demircioğlu";
+            k2.gozRengi = "Kahverengi";
+            k2.dogumYili = 1996;
+            k2.yazdir();
+            Kisi k3 = new Kisi("Nurgül", "Çümen", 1996, "Ela");
+            k3.yazdir();
+        }
+    }
+}
