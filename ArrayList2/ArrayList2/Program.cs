ac13d69469d3fae6cc4520ead6d8ceba31b1231c
PauAna <PauAna>
2017-02-21 19:03:16 +0300

    21.02.2017 tarihinde işlenen kodlar

---
+++ ArrayList2/ArrayList2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayList2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList a = new ArrayList();
+            a.Add(2);
+            a.Add(5);
+            a.Add("Şevket");
+            a.Add(3.141592);
+
+            Kisi k1 = new Kisi();
+            a.Add(k1);
+            a.Add(14);
+            yazdir(a);
+            Console.WriteLine("Elemanların toplamı:{0}", tamsayilariTopla(a));
+        }
+
+        static void yazdir(ArrayList al)
+        {
+            foreach (var eleman in al)
+            {
+                Console.WriteLine(eleman);
+            }
+        }
+
+        /*
+         * ArrayList içindeki tamsayı değerlerini toplayıp geri döndürürü
+         */
+        static int tamsayilariTopla(ArrayList al)
+        {
+            int toplam = 0;
+            foreach (var eleman in al)
+            {
+                //if(eleman.GetType() == typeof(int))
+                if (eleman is int)//eleman tamsayı ise
+                    toplam += (int)eleman;
+            }
+            return toplam;
+        }
+    }
+}
---
+++ GenericArrayList/GenericArrayList/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> l = new List<int>();
+            l.Add(5);
+            l.Add(7);
+            l.Add(26);
+            if(l.Contains(7))
+                Console.WriteLine("7 değeri mevcut");
+            yazdir(l);
+        }
+
+        static void yazdir(List<int> l)
+        {
+            //int toplam = 0;
+            foreach (var eleman in l)
+            {
+                //toplam += eleman;
+                Console.WriteLine(eleman);
+            }
+        }
+    }
+}
