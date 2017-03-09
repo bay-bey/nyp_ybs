3fd5e3fd2c230ebf7a9b6d0c48fda9974177e151
PauAna <PauAna>
2017-03-09 12:23:00 +0300

    09.03.2017 tarihinde derste işlenen kodlar

---
+++ HatirlaticiBO/HatirlaticiBO/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HatirlaticiBO
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Zaman z = new Zaman(11, 119, 285);
+            Zaman z1 = z.saniyeEkle(500);
+            Zaman z3 = new Zaman(01, 10, 25);
+            Zaman z2 = z.zamanEkle(z1);
+            Zaman z4 = z3.zamanEkle(z);
+        }
+    }
+}
---
+++ HatirlaticiBO/HatirlaticiBO/Zaman.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HatirlaticiBO
+{
+    class Zaman
+    {
+        private int saat;
+        private int dakika;
+        private int saniye;
+
+        public int Saat
+        {
+            get { return saat; }
+            set
+            {
+                saat = value % 24;
+            }
+        }
+
+        public int Dakika
+        {
+            get { return dakika; }
+            set
+            {
+                if (value >= 60)
+                {
+                    saat = value / 60;
+                }
+                dakika = value % 60;
+
+            }
+        }
+
+        public int Saniye
+        {
+            get { return saniye; }
+            set
+            {
+                if (value >= 60)
+                {
+                    dakika += value / 60;
+                }
+                saniye = value % 60;
+            }
+        }
+        public Zaman(int saat, int dakika, int saniye)
+        {
+            this.Saniye = saniye;
+            this.Dakika += dakika;
+            this.Saat += saat;
+        }
+
+        public Zaman saniyeEkle(int saniye)
+        {
+            Zaman donecek = new Zaman(Saat, Dakika, Saniye);
+            donecek.Saniye += saniye;
+            return donecek;
+        }
+        public Zaman zamanEkle(Zaman z)
+        {
+            Zaman donecek = new Zaman(Saat, Dakika, Saniye);
+            donecek.Saniye += z.Saniye;
+            donecek.Dakika += z.Dakika;
+            donecek.Saat += z.Saat;
+            return donecek;
+        }
+    }
+}
